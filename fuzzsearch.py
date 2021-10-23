997cce7463d7dd9af4d6d61f309be576790d533b
Kirash351 <90014269+Kirash351@users.noreply.github.com>
2021-10-23 16:24:23 -0400

    optimize search results

--- fuzzsearch.py
+++ fuzzsearch.py
@@ -2,17 +2,21 @@ from thefuzz import fuzz
 
 
 def SearchRestaurant(restaurant_list, input):
+    result = []
     for i in restaurant_list:
         if fuzz.token_set_ratio(i, input) == 100:
-            return i
+            result.append(i)
     print("Your search - " + input + """ - did not match any restaurants.\n
 Suggestions:\n
 Make sure all words are spelled correctly.\n
 Try different keywords.\n
 Try more general keywords.""")
-    return None
+    if len(result) > 0:
+        return result
+    else:
+        return
 
 
 restaurant_list = ["Shake Shack", "Jane's Kitechen", "A.S.K Tea"]
-input = "boba"
-SearchRestaurant(restaurant_list, input)
+input = "Shake shack"
+print(SearchRestaurant(restaurant_list, input))
