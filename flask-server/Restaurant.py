2c16e9b25ab92b0c031b9fa4b8da79338ed3f7c8
Alice Huang <alice.huangqian97@gmail.com>
2021-10-23 16:08:33 -0400

    Initial complete of restaurant API calls and dummy values

---
+++ flask-server/Restaurant.py
@@ -0,0 +1,29 @@
+from hmacHelper import request
+import json
+
+
+def postRestaurants():
+    url = "https://gateway-staging.ncrcloud.com/site/sites"
+
+    restaurants = []
+    with open("Restaurants.json") as json_file:
+        data = json.load(json_file)
+        for restaurant in data["restaurants"]:
+            payload = json.dumps(restaurant)
+            response = request(url, "POST", payload)
+            restaurants.append(response["data"])
+    return restaurants
+
+
+def getRestaurants():
+    restaurants = []
+    ref_ids = ["site-001", "site-002"]
+    url = "https://gateway-staging.ncrcloud.com/site/sites/by-reference-id/"
+    for ref_id in ref_ids:
+        restaurant = request(url + ref_id, "GET", {})
+        restaurants.append(restaurant)
+        print(restaurant)
+    return restaurants
+
+
+print(getRestaurants())
