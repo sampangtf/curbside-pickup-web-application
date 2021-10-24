cdd1ed7b1a2ef7e52bf1d621a8a169faed35853e
Kirash351 <kirashijia@gmail.com>
2021-10-23 23:42:57 -0400

    Update Customer

--- flask-server/Customer.py
+++ flask-server/Customer.py
@@ -3,7 +3,7 @@ from hmacHelper import request
 
 
 def CreateCustomer():
-    url = "https://api.ncr.com/cdm/consumers"
+    url = "https://gateway-staging.ncrcloud.com/cdm/consumers"
 
     customers = []
     with open("data.json") as json_file:
@@ -15,11 +15,20 @@ def CreateCustomer():
     return customers
 
 
+def GetCustomer():
+    requestURL = "https://gateway-staging.ncrcloud.com/cdm/consumers/"
+    accountNumber = "2JPOAAUBWC1FMQTP"
+    httpMethod = "GET"
+    payload = {}
+    res = request(requestURL + accountNumber, httpMethod, payload)
+    return res["data"]
+
+
 def CreateCustomAttributeSet():
-    url = "https://api.ncr.com/site/v1/extensions"
+    url = "https://gateway-staging.ncrcloud.com/site/v1/extensions"
     with open("data.json") as json_file:
         data = json.load(json_file)
-        payload = data['custom attribute']
+        payload = json.dumps(data['custom attribute'])
         print(payload)
     response = request(url, "POST", payload)
     return response
