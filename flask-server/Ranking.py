98372742e26345003ca4ac676b87aee8c69f28ab
Sam TF Pang <81877462+sampangtf@users.noreply.github.com>
2021-10-23 21:52:00 -0400

    update json's indexing

--- flask-server/Ranking.py
+++ flask-server/Ranking.py
@@ -1,27 +1,27 @@
 from googlemap import distance_matrix
 
-def restaurant_rank(search_results, origin, beta):
-    
-    destinations = 
-    #distance_list = list()
-    # for idx in range(n_restaurant):
-    #     traveltime = distance_matrix['rows'][0]['elements'][idx]['duration']['value']
-    #     distance_list.append(traveltime)
-    destination_list = search_results['name']
-    _distance_matrix = distance_matrix(origin, destination_list, depature_time = "now")
-    traveltime_list= [_distance_matrix['rows'][0]['elements'][idx]['duration']['value'] for idx in len(distance_matrix['rows'][0]['elements'])]
-    traveltime_score = list(map(lambda x: 1/x, traveltime_list))
-    norm_traveltime_score = [(float(i) - min (traveltime_score))/(max(traveltime_score) - min(traveltime_score)) for i in traveltime_score]
+def restaurant_rank(restaurants, origin, beta):
 
-    
-    total_n_rating = sum(search_results['n_rating'])
-    rating = (search_results['rating'] * search_results['n_rating'])/ total_n_rating
-    norm_rating = [(float(i) - min(rating))/(max(rating)-min(rating)) for i in rating]
+    destination_list = [res['siteName'] for res in restaurants]
+
+    _distance_matrix = distance_matrix(origin, destination_list, depature_time = "now")
+
+    ####Getting Travel Time Score
+    traveltime_list= [_distance_matrix['rows'][0]['elements'][idx]['duration']['value'] for idx in len(_distance_matrix['rows'][0]['elements'])]
+    traveltime_score = list(map(lambda x: 1/x, traveltime_list))
+    scaled_traveltime_score = [(float(i) - min (traveltime_score))/(max(traveltime_score) - min(traveltime_score)) for i in traveltime_score]
+
+    ####Getting 
+    rating_list = [(res['rating'], res['noRating']) for res in restaurants]
+    total_n_rating = sum(rating_list)
+    rating_list_norm = [rating * noRating/total_n_rating for rating, noRating in rating_list]
+    #rating_list_norm = (restaurants['rating'] * restaurants['n_rating'])/ total_n_rating
+    scaled_rating = [(float(i) - min(rating_list_norm))/(max(rating_list_norm)-min(rating_list_norm)) for i in rating_list_norm]
 
     weighted_score_dict = {}
-    for idx, result in enumerate(search_results):
-        name = search_results['name']
-        weighted_score = norm_rating * norm_traveltime_score
+    for idx, restuarnt in enumerate(restaurants):
+        name = destination_list[idx]
+        weighted_score = scaled_rating[idx] * scaled_traveltime_score[idx]
         weighted_score_dict['name'] = weighted_score
 
     sorted_weighted_score_dict = {k:v for k,v in sorted(weighted_score_dict.items(), key=lambda item: item[1])}
